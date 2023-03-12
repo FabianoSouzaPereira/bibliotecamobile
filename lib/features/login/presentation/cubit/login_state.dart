6220ba3ba457cce04c8f4c72a3a0d2003b7130f2
Fabiano <fabiano.pereira@seventh.com.br>
2023-03-11 22:17:10 -0300

    add cubit and state code to Login

--- lib/features/login/presentation/cubit/login_state.dart
+++ lib/features/login/presentation/cubit/login_state.dart
@@ -1,1 +1,34 @@
+part of 'login_cubit.dart';
 
+abstract class LoginState extends Equatable {
+  const LoginState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class LoginInitial extends LoginState {}
+
+class Empty extends LoginState {}
+
+class Loading extends LoginState {}
+
+class Loaded extends LoginState {
+  final LoginState login;
+
+  Loaded({required this.login});
+
+  @override
+  List<Object> get props => [login];
+}
+
+class Error extends LoginState {
+  final String message;
+
+  Error({required this.message});
+
+  @override
+  List<Object> get props => [message];
+}
+
+class Unknown extends LoginState {}
---
+++ lib/features/login/presentation/widgets/login_display.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class HomeDisplay extends StatelessWidget {
+  const HomeDisplay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
